bd897c565fd71a54d97675481416b865ff20e576
djebar <82457196+abdeldjabarbell@users.noreply.github.com>
2024-04-14 01:26:29 +0100

    Add files via upload

--- miraProduct.js
+++ miraProduct.js
@@ -53,9 +53,9 @@ async function afficherDetailsProduit(productId, storeName, collect_p) {
         const image3=detailsProduit.imageUrl_produit_3;
         const image4=detailsProduit.imageUrl_produit_4;
         const discreption =detailsProduit.Description;
-        const id1 = detailsProduit.idproduitSimilaire1;
-        const id2 = detailsProduit.idproduitSimilaire2;
-        const id3 = detailsProduit.idproduitSimilaire3;
+        const id1 = detailsProduit.idproduit_Similaire1;
+        const id2 = detailsProduit.idproduit_Similaire2;
+        const id3 = detailsProduit.idproduit_Similaire3;
 
         const images = [image1, image2, image3, image4];
 
@@ -170,7 +170,7 @@ async function afficherDetailsProduit(productId, storeName, collect_p) {
         const priceOriginale = price;
         const pricePromo = parseFloat((priceOriginale * (promotion / 100)).toFixed(2));
 
-
+        const acheter_btn1 = document.getElementById('acheter_btn1');
         if(promotion>0){
             const prixstyel = document.createElement('p');
             prixstyel.className="prixstyel";
@@ -181,12 +181,15 @@ async function afficherDetailsProduit(productId, storeName, collect_p) {
             prixstyel_promo.className="prixstyel_promo";
             prixstyel_promo.innerHTML= priceOriginale+"DA";
             prix_prixpromo.appendChild(prixstyel_promo);
+            acheter_btn1.innerHTML=pricePromo+ " DA";
         }
         if(promotion===0){
             const prixstyel = document.createElement('p');
             prixstyel.className="prixstyel";
             prixstyel.innerHTML= priceOriginale+"DA";
             prix_prixpromo.appendChild(prixstyel);
+            acheter_btn1.innerHTML=priceOriginale+ " DA";
+
         }
 
         
@@ -251,8 +254,6 @@ async function afficherDetailsProduit(productId, storeName, collect_p) {
         }); 
         
 
-
-
         function showMore() {
             const pElement = document.querySelector('.dicreptionstyel');
             const text = pElement.textContent;
@@ -281,6 +282,195 @@ async function afficherDetailsProduit(productId, storeName, collect_p) {
         }
         showMore();
 
+        const titre_of_page4 = document.createElement('h1');
+        titre_of_page4.className="titre_of_page";
+        titre_of_page4.innerHTML="La disponibilité : ";
+        produitDetailles_bg.appendChild(titre_of_page4);
+
+        const disponibilite_ = document.createElement('p');
+        disponibilite_.className = "dicreptionstyel";
+
+        
+        if (quantiteproduit === 0) {
+            disponibilite_.innerHTML = "Non disponible";
+            disponibilite_.style.color = "red";
+
+            acheter_btn1.style.color=" rgba(115, 115, 114, 0.709)";  
+            acheter_btn1.style.border="1px solid  rgba(115, 115, 114, 0.709)";
+            acheter_btn1.disabled = false;
+
+        } else if (quantiteproduit < 10 && quantiteproduit > 0) {
+            disponibilite_.innerHTML = "Limité : " + quantiteproduit;
+            disponibilite_.style.color = "red";
+        } else {
+            disponibilite_.innerHTML = "Disponible";
+            disponibilite_.style.color = "green";
+        }
+        
+        produitDetailles_bg.appendChild(disponibilite_);
+
+        const titre_of_page5 = document.createElement('h1');
+        titre_of_page5.className="titre_of_page";
+        titre_of_page5.innerHTML="Produit similaire : ";
+        produitDetailles_bg.appendChild(titre_of_page5);
+
+
+        const prdct_simlr = [id1,id2,id3];
+
+        for(let p=0 ; p<3;p++){
+            const docRef = doc(db, 'items', storeName, 'produits', collect_p, 'produits', prdct_simlr[p]);
+            const collection_produit = collect_p;
+
+            const docSnap = await getDoc(docRef);
+            if (docSnap.exists()) {
+                
+                const data = docSnap.data(); // Correction ici
+                     
+                // Récupération des données
+                const image_produit = data.imageUrl_produit_1;
+                const time_produit = data.timestamp; // Conversion en objet Date
+
+                const titre_de_produit = data.Titre;
+                const prix_de_produit = data.prix;
+                const promotion_produit = data.promotion;
+                const colors_produit = data.colors;
+                const n_colors_produit = data.colors_number;
+                const quantiteproduit = data.quantiteproduit;
+
+
+                const items_dispo = document.querySelector(".items_dispo");
+                const bg_item = document.createElement("div");
+                bg_item.className = 'bg_item';
+            
+                const bg_item_img = document.createElement("div");
+                bg_item_img.className = "bg_item_img";
+                const img_produi = document.createElement("img");
+                img_produi.src = image_produit;
+
+                const time_produit_mj = new Date(time_produit.seconds * 1000);
+                const dateNow = new Date();
+
+                const differenceInMillis = Math.abs(time_produit_mj - dateNow); // Différence en millisecondes
+                const differenceInDays = differenceInMillis / (1000 * 60 * 60 * 24); // Conversion en jours
+                  
+
+
+                if (differenceInDays < 4) { 
+                    const new_produit = document.createElement("div");
+                    new_produit.className = "new_produit";
+                    const p_new_produit = document.createElement("p");
+                    p_new_produit.innerText = "Nouveau";
+                    new_produit.appendChild(p_new_produit);
+                    bg_item_img.appendChild(new_produit);
+                }
+                if (quantiteproduit > 0 && quantiteproduit < 10) { 
+                    const limited_edition = document.createElement("div");
+                    limited_edition.className = "limited_edition";
+                    const p_limited_edition = document.createElement("p");
+                    p_limited_edition.innerText = "Édition limitée : " + quantiteproduit;
+                    limited_edition.appendChild(p_limited_edition);
+                    bg_item_img.appendChild(limited_edition);
+
+                } else if (quantiteproduit === 0) { 
+                    const limited_edition = document.createElement("div");
+                    limited_edition.className = "limited_edition";
+                    const p_limited_edition = document.createElement("p");
+                    p_limited_edition.innerText = "non disponible";
+                    limited_edition.appendChild(p_limited_edition);
+                    bg_item_img.appendChild(limited_edition);
+                }
+                
+
+
+                
+            
+                bg_item_img.appendChild(img_produi);
+                bg_item.appendChild(bg_item_img);
+
+                const colleection_name = document.createElement("div");
+                colleection_name.className = "colleection_name";
+                const p_colleection_name = document.createElement("p");
+                p_colleection_name.innerText = collection_produit ;
+                colleection_name.appendChild(p_colleection_name);
+                bg_item_img.appendChild(colleection_name);
+            
+                // Titre du produit
+                const bg_item_titre = document.createElement("div");
+                bg_item_titre.className = "bg_item_titre";
+                const p_bg_item_titre = document.createElement("p");
+                p_bg_item_titre.innerHTML = titre_de_produit;
+                p_bg_item_titre.className="p_bg_item_titre";
+                bg_item_titre.appendChild(p_bg_item_titre);
+                bg_item.appendChild(bg_item_titre);
+            
+                // Prix du produit
+                const bg_item_prix = document.createElement("div");
+                bg_item_prix.className = "bg_item_prix";
+            
+                if (promotion_produit === 0) { // Pas de promotion
+                    const p_bg_item_prix_originale = document.createElement("p");
+                    p_bg_item_prix_originale.innerHTML = prix_de_produit+"DA";
+                    p_bg_item_prix_originale.className = "p_bg_item_prix_originale";
+
+                    bg_item_prix.appendChild(p_bg_item_prix_originale);
+                } else { // Avec promotion
+                    const promotion_bar = document.createElement("div");
+                    promotion_bar.className = "promotion_bar";
+                    const p_promotion = document.createElement("p");
+                    p_promotion.innerHTML = "Promotion";
+                    promotion_bar.appendChild(p_promotion);
+                    bg_item_img.appendChild(promotion_bar);
+            
+                    const nouveauPrix = prix_de_produit * (1 - promotion_produit / 100);
+                    const p_bg_item_prix_originale = document.createElement("p");
+                    p_bg_item_prix_originale.className = "p_bg_item_prix_originale";
+                    p_bg_item_prix_originale.innerHTML = nouveauPrix+"DA"; // Affichage avec deux décimales
+                    bg_item_prix.appendChild(p_bg_item_prix_originale);
+                    //ancien prix
+                    const p_bg_item_prix_promotion = document.createElement("p");
+                    p_bg_item_prix_promotion.innerHTML = prix_de_produit +"DA";
+                    p_bg_item_prix_promotion.className = "p_bg_item_prix_promotion";
+                    bg_item_prix.appendChild(p_bg_item_prix_promotion);
+
+                }
+            
+                bg_item.appendChild(bg_item_prix);
+            
+                // Couleurs du produit
+                const bg_item_coleurs = document.createElement("div");
+                bg_item_coleurs.className = "bg_item_coleurs";
+                for (let i = 0; i < n_colors_produit; i++) {
+                    const color_dispo = document.createElement("div");
+                    color_dispo.className = "color_dispo";
+                    color_dispo.style.backgroundColor = colors_produit[i];
+                    bg_item_coleurs.appendChild(color_dispo);
+                }
+                bg_item.appendChild(bg_item_coleurs);
+                items_dispo.appendChild(bg_item);
+            
+                bg_item.addEventListener('click', () => {
+                    // Redirection vers la page du produit avec les paramètres nécessaires
+                    window.location.href = `miraProduct.html?store=${storeName}&collection_pr=${collection_produit}&id=${prdct_simlr[p]}`;
+                });  
+         
+            
+            } else {
+                console.log("Aucun document trouvé avec cet ID de produit.");
+                return null;
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
  } else {
         // Gérer le cas où aucun détail de produit n'est trouvé
         const errorDiv = document.createElement('div');
@@ -305,6 +495,12 @@ window.onload = () => {
     }
 };
 
+const logo_image = document.getElementById("logo_image");
+
+logo_image.addEventListener('click', function() {
+    window.location.href = `mira_stors.html?`; // Redirection vers la page du produit avec l'ID du produit
+    //window.history.back();
+});
 
 
 
